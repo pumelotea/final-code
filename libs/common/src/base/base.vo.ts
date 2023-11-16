cfa479442bc204dab43491d50da5c9f88ca07929
pumelotea <535553297@qq.com>
2023-11-16 21:41:56 +0800

    upd: base.vo.ts

--- libs/common/src/base/base.vo.ts
+++ libs/common/src/base/base.vo.ts
@@ -5,27 +5,27 @@ export class BaseVo {
    * 创建时间
    */
   @VoDate()
-  createdAt?: Date;
+  createdAt?: Date | null;
   /**
    * 创建者
    */
-  createdBy?: string;
+  createdBy?: string | null;
   /**
    * 更新时间
    */
   @VoDate()
-  updatedAt?: Date;
+  updatedAt?: Date | null;
   /**
    * 更新者
    */
-  updatedBy?: string;
+  updatedBy?: string | null;
   /**
    * 删除时间
    */
   @VoDate()
-  deleted?: Date;
+  deleted?: Date | null;
   /**
    * 删除者
    */
-  deletedBy?: string;
+  deletedBy?: string | null;
 }
